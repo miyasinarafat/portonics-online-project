eedc61ddb56430d51fa047d295b68bd87f53e919
miyasinarafat <miyasinarafat@gmail.com>
2024-05-12 01:25:12 +0600

    add login api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,17 +2,50 @@
 
 namespace App\Http\Controllers;
 
-class ExampleController extends Controller
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+final class UserController extends Controller
 {
     /**
-     * Create a new controller instance.
+     * Get token via credentials
      *
-     * @return void
+     * @param Request $request
+     * @return JsonResponse
+     * @throws ValidationException
      */
-    public function __construct()
+    public function login(Request $request): JsonResponse
     {
-        //
+        $this->validate($request, [
+            'email' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        $credentials = $request->only(['email', 'password']);
+
+        if (! $token = Auth::attempt($credentials)) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
     }
 
-    //
+    /**
+     * Get the token array structure.
+     *
+     * @param string $token
+     *
+     * @return JsonResponse
+     */
+    protected function respondWithToken(string $token): JsonResponse
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'user' => auth()->user(),
+            'expires_in' => auth()->factory()->getTTL() * 60 * 24
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,10 @@
 <?php
 
-/** @var \Laravel\Lumen\Routing\Router $router */
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
+use Laravel\Lumen\Routing\Router;
+/** @var Router $router */
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->post('login', ['as' => 'login', 'uses' => 'UserController@login']);
