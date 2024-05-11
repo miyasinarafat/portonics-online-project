21d0e4cd8f586359dd3988cf3b2c0f8d8d2192af
miyasinarafat <miyasinarafat@gmail.com>
2024-05-11 14:42:30 +0600

    configure model and migration for auth

---
+++ app/Models/User.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Lumen\Auth\Authorizable;
+
+class User extends Model implements AuthenticatableContract, AuthorizableContract
+{
+    use Authenticatable, Authorizable, HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'address',
+        'email_verified_at',
+        'is_admin',
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var string[]
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'address' => 'json',
+        'email_verified_at' => 'datetime',
+        'is_admin' => 'boolean',
+        'password' => 'hashed',
+    ];
+}
