c4db713430d2ebae2ae597f728adcc63ba9b1c21
miyasinarafat <miyasinarafat@gmail.com>
2024-05-12 01:23:32 +0600

    add admin seeder

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create admin
+        User::createOrFirst(
+            [
+                'email' => 'admin@admin.com',
+            ],
+            [
+                'name' => 'Arafat',
+                'is_admin' => 1,
+                'password' => Hash::make('password'),
+            ]
+        );
+    }
+}
