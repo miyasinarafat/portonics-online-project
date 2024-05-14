832d1acea3a44df2764a1186b583039119021be3
miyasinarafat <miyasinarafat@gmail.com>
2024-05-14 17:08:19 +0600

    prep order models, migrations

---
+++ app/Models/Order.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\OrderStatusEnum;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'user_id',
+        'order_time',
+        'order_code',
+        'amount',
+        'currency',
+        'status_idx',
+        'invoice_id',
+        'invoice_url',
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected $casts = [
+        'order_time' => 'datetime',
+        'status_idx' => OrderStatusEnum::class,
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    /**
+     * @return BelongsToMany
+     */
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            Product::class,
+            'order_items',
+            'order_id',
+            'product_id',
+        );
+    }
+}
